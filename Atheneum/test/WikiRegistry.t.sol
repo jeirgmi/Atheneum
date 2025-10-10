beb244081fe50e4da04ac1036021df6a977e26e8
Jeir <jackbrightdefi@gmail.com>
2025-10-10 02:04:56 +0100

    Updated WikiRegistry and test files

--- Atheneum/test/WikiRegistry.t.sol
+++ Atheneum/test/WikiRegistry.t.sol
@@ -1,31 +1,56 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/WikiRegistry.sol";
 
 contract WikiRegistryTest is Test {
     WikiRegistry registry;
-    address alice = address(0xABCD);
 
     function setUp() public {
         registry = new WikiRegistry();
     }
 
-    function testCreateArticleAndSubmitContribution() public {
-        vm.prank(alice);
+    function testCreateArticle() public {
         uint256 articleId = registry.createArticle("Intro to Dust", "cid://intro");
 
-        (uint256 id, string memory title, string memory cid, address author, uint256 createdAt, bool exists) = registry.articles(articleId);
+        (
+            uint256 id,
+            string memory title,
+            string memory cid,
+            address author,
+            uint256 createdAt,
+            bool exists
+        ) = registry.articles(articleId);
+
+        assertEq(id, articleId);
         assertEq(title, "Intro to Dust");
-        assertEq(author, alice);
+        assertEq(cid, "cid://intro");
+        assertEq(author, address(this));
+        assertTrue(exists);
+    }
 
-        vm.prank(alice);
-        uint256 contribId = registry.submitContribution(articleId, "cid://edit1", 1);
+    function testAddContribution() public {
+        uint256 articleId = registry.createArticle("Test Article", "cid://test");
+        uint256 contribId = registry.addContribution(articleId, "cid://update", 1);
 
-        (uint256 cidId, uint256 aId, address contributor, string memory cCid, uint8 ctype, uint256 submittedAt, bool approved, bool rewarded) = registry.contributions(contribId);
+        (
+            uint256 id,
+            uint256 aId,
+            address contributor,
+            string memory cCid,
+            uint8 ctype,
+            uint256 submittedAt,
+            bool approved,
+            bool rewarded
+        ) = registry.contributions(contribId);
+
+        assertEq(id, contribId);
         assertEq(aId, articleId);
-        assertEq(contributor, alice);
-        assertEq(cCid, "cid://edit1");
+        assertEq(contributor, address(this));
+        assertEq(cCid, "cid://update");
+        assertEq(ctype, 1);
+        assertFalse(approved);
+        assertFalse(rewarded);
     }
 }
